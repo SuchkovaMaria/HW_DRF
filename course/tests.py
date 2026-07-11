11384a8c34d88f754a0c6c1d9418ad9cdf9fc6d7
Mariya_Suchkova <mashakraxmal@gmail.com>
2026-07-11 23:15:55 +0300

    test 11 ci

--- course/tests.py
+++ course/tests.py
@@ -32,7 +32,7 @@ class LessonTestCase(APITestCase):
     def test_lesson_create_valid(self):
         url = reverse("course:lesson-create")
         data = {"name": "test_leasson_two", "course": self.course.pk, "video_path": "https://youtube//test2", "owner": self.user.pk}
-        response = self.client.post(url, data)
+        response = self.client.post(url, data, format='json')
         print("\nОШИБКИ ВАЛИДАЦИИ:", response.data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(Lesson.objects.all().count(), 1)
@@ -71,7 +71,7 @@ class SubscriptionsTestCase(APITestCase):
     def test_subscriptions_create_error(self):
         url = reverse("course:subscriptions-create", args=[self.course.pk])
         data = {"course": self.course.pk, "owner": self.user.pk}
-        response = self.client.post(url, data)
+        response = self.client.post(url, data, format='json')
         print("\nОШИБКИ ВАЛИДАЦИИ:", response.data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(Subscriptions.objects.all().count(), 1)
